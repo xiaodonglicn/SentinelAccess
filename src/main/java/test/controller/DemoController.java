fc27061b04c094e7ccf951245fd81517f569b7fe
xiaodonglicn <1158672373@qq.com>
2025-07-04 15:45:10 +0800

    降级

--- src/main/java/test/controller/DemoController.java
+++ src/main/java/test/controller/DemoController.java
@@ -10,14 +10,9 @@ public class DemoController {
 
     // 接口A - QPS限流为2
     @GetMapping("/api/a")
-    @SentinelResource(value = "apiA", blockHandler = "handleApiABlock")
+    @SentinelResource(value = "apiA", blockHandler = "handleApiABlock", fallback = "fallback")
     public String apiA() {
         return "This is API A response";
-    }
-
-    // 接口A的限流处理方法
-    public String handleApiABlock(BlockException ex) {
-        return "API A is blocked by Sentinel. Too many requests!";
     }
 
     // 接口B - QPS限流为5
@@ -27,8 +22,18 @@ public class DemoController {
         return "This is API B response";
     }
 
+    // 接口A的限流处理方法
+    public String handleApiABlock(BlockException ex) {
+        return "API A is blocked by Sentinel. Too many requests!";
+    }
+
     // 接口B的限流处理方法
     public String handleApiBBlock(BlockException ex) {
         return "API B is blocked by Sentinel. Too many requests!";
+    }
+
+    // 降级处理方法,当apiA抛出非BlockException的异常触发降级
+    public String fallback(Throwable ex) {
+        return "降级A";
     }
 }
